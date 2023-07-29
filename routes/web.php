f11df6800b9b14dcee2bc2f13fb4c732c8a9fe85
David Dieperink <david.dieperink@ik.me>
2023-07-29 20:02:07 +0200

    updated double quote

--- routes/web.php
+++ routes/web.php
@@ -21,7 +21,7 @@ Route::get('/', function () {
 Route::prefix('/blog')->name('blog.')->group(function () {
     Route::get('/', function (Request $request) {
         return [
-            "link" => \route("blog.show", ['slug' => 'article', "id" => 11]),
+            "link" => \route("blog.show", ["slug" => 'article', "id" => 11]),
         ];
     })->name('index');
 
@@ -32,7 +32,7 @@ Route::prefix('/blog')->name('blog.')->group(function () {
             "name" => $request->input('name')
         ];
     })->where([
-        'id' => '[0-9]+',
-        'slug' => '[a-z0-9\-]+',
+        "id" => '[0-9]+',
+        "slug" => '[a-z0-9\-]+',
     ])->name('show');
 });
