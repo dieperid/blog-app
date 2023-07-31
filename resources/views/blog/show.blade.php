7cd62b1f046156531d2313e21f64deb8aeb7ecde
David Dieperink <david.dieperink@ik.me>
2023-07-31 15:45:57 +0200

    new page for the post

---
+++ resources/views/blog/show.blade.php
@@ -0,0 +1,12 @@
+@extends('base')
+
+@section('title', $post->title)
+
+@section('content')
+    <article>
+        <h2 class="text-3xl">{{ $post->content }}</h2>
+        <p class="mb-3">
+            {{ $post->content }}
+        </p>
+    </article>
+@endsection
