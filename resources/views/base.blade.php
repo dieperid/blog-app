57fd91c993a67940301eca4961ad6d4efce911e4
David Dieperink <david.dieperink@ik.me>
2023-07-31 14:37:54 +0200

    added a navbar with tailwind

--- resources/views/base.blade.php
+++ resources/views/base.blade.php
@@ -5,12 +5,27 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>My blog</title>
+    <title>@yield('title')</title>
+    <script src="https://cdn.tailwindcss.com"></script>
 </head>
 
 <body>
+    <nav class="bg-white shadow dark:bg-gray-800">
+        <div class="container flex items-center justify-center p-6 mx-auto text-gray-600 capitalize dark:text-gray-300">
+            <a href="#" class="text-gray-800 dark:text-gray-200 border-b-2 border-blue-500 mx-1.5 sm:mx-6">Home</a>
+
+            <a href="#"
+                class="border-b-2 border-transparent hover:text-gray-800 dark:hover:text-gray-200 hover:border-blue-500 mx-1.5 sm:mx-6">Link</a>
+
+            <a href="#"
+                class="border-b-2 border-transparent hover:text-gray-800 dark:hover:text-gray-200 hover:border-blue-500 mx-1.5 sm:mx-6">pricing</a>
+
+            <a href="#"
+                class="border-b-2 border-transparent hover:text-gray-800 dark:hover:text-gray-200 hover:border-blue-500 mx-1.5 sm:mx-6">blog</a>
+        </div>
+    </nav>
     <div class="container">
-        @yield('content');
+        @yield('content')
     </div>
 </body>
 
--- resources/views/blog/index.blade.php
+++ resources/views/blog/index.blade.php
@@ -1,4 +1,7 @@
-@extends('base');
+@extends('base')
+
+@section('title', 'Welcome of the blog')
+
 @section('content')
     <h1>My blog</h1>
 @endsection
